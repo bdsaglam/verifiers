e2d123460938d29ec13c4dfef15f4e932e806e4d
Barış Deniz Sağlam <bdsaglam@gmail.com>
2025-03-12 20:33:26 +0300

    update musique preprocessing

--- src/verifiers/datasets/musique.py
+++ src/verifiers/datasets/musique.py
@@ -1,3 +1,5 @@
+import random
+
 from datasets import Dataset
 
 
@@ -10,14 +12,37 @@ def _make_doc(p: dict) -> dict:
 
 
 def preprocess_dataset(dataset: Dataset) -> Dataset:
-    dataset = dataset.filter(lambda x: len(x["question_decomposition"]) <= 2)
-    return dataset.map(
+    dataset = dataset.filter(lambda x: len(x["question_decomposition"]) > 2)
+    dataset = dataset.map(
         lambda x: {
             "prompt": [{"role": "user", "content": x["question"]}],
+            # TODO: Include all paragraphs
             "docs": [_make_doc(p) for p in x["paragraphs"]],
             "answer": x["answer"],
             "answers": [x["answer"], *x["answer_aliases"]],
-            "supporting_titles": [p["title"] for p in x["paragraphs"] if p["is_supporting"]],
+            "supporting_titles": [
+                p["title"] for p in x["paragraphs"] if p["is_supporting"]
+            ],
         },
-        remove_columns=["id", "question", "paragraphs", "question_decomposition", "answerable", "answer_aliases"],
+        remove_columns=[
+            "id",
+            "question",
+            "paragraphs",
+            "question_decomposition",
+            "answerable",
+            "answer_aliases",
+        ],
     )
+
+    # TODO: Include all paragraphs
+    dataset = dataset.map(
+        lambda x: {
+            "docs": [
+                doc
+                for doc in x["docs"]
+                if doc["is_supporting"] or random.random() < 0.2
+            ]
+        }
+    )
+
+    return dataset
