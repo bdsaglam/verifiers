a64060e8436e82d932b310ab4d465fe6345caac2
Barış Deniz Sağlam <bdsaglam@gmail.com>
2025-03-27 00:02:34 +0300

    add more exps

--- nbs/run.sh
+++ nbs/run.sh
@@ -1,14 +1,2 @@
 #!/bin/sh
 
-dvc exp run --queue \
-    -S dataset.path='bdsaglam/musique-mini' \
-    -S dataset.name='answerable' \
-    -S dataset.split='validation' \
-    -S model.path='/home/baris/repos/verifiers/outputs/Llama-3.1-8B-Instruct-ragent-grpo-musique-scaled-merged' \
-    -S model.temperature='0.5' \
-    -S model.top_p='0.95' \
-    -S retriever.name='semantic' \
-    -S retriever.top_k='1' \
-    -S run='1' \
-    -S devices='0'
-
