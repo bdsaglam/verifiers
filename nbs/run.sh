6d908235173840c33cb22a8ecbba2c07d0e91643
Barış Deniz Sağlam <bdsaglam@gmail.com>
2025-03-26 10:15:08 +0300

    prepare exps

--- nbs/run.sh
+++ nbs/run.sh
@@ -1,2 +1,68 @@
 #!/bin/sh
 
+dvc exp run --queue \
+    -S dataset.path='bdsaglam/musique-mini' \
+    -S dataset.name='answerable' \
+    -S dataset.split='validation' \
+    -S model.name='/home/baris/repos/verifiers/outputs/Llama-3.1-8B-Instruct-ragent-grpo-musique-merged/home/baris/repos/verifiers/outputs/Llama-3.1-8B-Instruct-ragent-grpo-musique-scaled-merged' \
+    -S model.temperature='0.5' \
+    -S model.top_p='0.95' \
+    -S retriever.name='hybrid' \
+    -S retriever.top_k='1' \
+    -S run='1'
+
+dvc exp run --queue \
+    -S dataset.path='bdsaglam/musique-mini' \
+    -S dataset.name='answerable' \
+    -S dataset.split='validation' \
+    -S model.name='/home/baris/repos/verifiers/outputs/Llama-3.1-8B-Instruct-ragent-grpo-musique-merged/home/baris/repos/verifiers/outputs/Llama-3.1-8B-Instruct-ragent-grpo-musique-scaled-merged' \
+    -S model.temperature='0.5' \
+    -S model.top_p='0.95' \
+    -S retriever.name='lexical' \
+    -S retriever.top_k='2' \
+    -S run='1'
+
+dvc exp run --queue \
+    -S dataset.path='bdsaglam/musique-mini' \
+    -S dataset.name='answerable' \
+    -S dataset.split='validation' \
+    -S model.name='/home/baris/repos/verifiers/outputs/Llama-3.1-8B-Instruct-ragent-grpo-musique-merged/home/baris/repos/verifiers/outputs/Llama-3.1-8B-Instruct-ragent-grpo-musique-scaled-merged' \
+    -S model.temperature='0.5' \
+    -S model.top_p='0.95' \
+    -S retriever.name='lexical' \
+    -S retriever.top_k='1' \
+    -S run='1'
+
+dvc exp run --queue \
+    -S dataset.path='bdsaglam/musique-mini' \
+    -S dataset.name='answerable' \
+    -S dataset.split='validation' \
+    -S model.name='/home/baris/repos/verifiers/outputs/Llama-3.1-8B-Instruct-ragent-grpo-musique-merged/home/baris/repos/verifiers/outputs/Llama-3.1-8B-Instruct-ragent-grpo-musique-scaled-merged' \
+    -S model.temperature='0.5' \
+    -S model.top_p='0.95' \
+    -S retriever.name='hybrid' \
+    -S retriever.top_k='3' \
+    -S run='1'
+
+dvc exp run --queue \
+    -S dataset.path='bdsaglam/musique-mini' \
+    -S dataset.name='answerable' \
+    -S dataset.split='validation' \
+    -S model.name='/home/baris/repos/verifiers/outputs/Llama-3.1-8B-Instruct-ragent-grpo-musique-merged/home/baris/repos/verifiers/outputs/Llama-3.1-8B-Instruct-ragent-grpo-musique-scaled-merged' \
+    -S model.temperature='0.5' \
+    -S model.top_p='0.95' \
+    -S retriever.name='hybrid' \
+    -S retriever.top_k='2' \
+    -S run='1'
+
+dvc exp run --queue \
+    -S dataset.path='bdsaglam/musique-mini' \
+    -S dataset.name='answerable' \
+    -S dataset.split='validation' \
+    -S model.name='/home/baris/repos/verifiers/outputs/Llama-3.1-8B-Instruct-ragent-grpo-musique-merged/home/baris/repos/verifiers/outputs/Llama-3.1-8B-Instruct-ragent-grpo-musique-scaled-merged' \
+    -S model.temperature='0.5' \
+    -S model.top_p='0.95' \
+    -S retriever.name='lexical' \
+    -S retriever.top_k='3' \
+    -S run='1'
+
