2e99c79876ce127c0b240f5adbee073cc27b0d56
Barış Deniz Sağlam <bdsaglam@gmail.com>
2025-03-29 19:16:51 +0300

    add new exps

--- nbs/run.sh
+++ nbs/run.sh
@@ -1,54 +1,6 @@
 #!/bin/sh
 
 dvc exp run --queue \
-    -S model.path='Qwen/Qwen2.5-1.5B-Instruct' \
-    -S model.temperature='0.5' \
-    -S model.top_p='0.95' \
-    -S retriever.name='hybrid' \
-    -S retriever.top_k='1' \
-    -S run='1' \
-    -S dataset.path='bdsaglam/hotpotqa-distractor' \
-    -S dataset.name='default' \
-    -S dataset.split='"validation[:300]"' \
-    -S devices='"0"'
-
-dvc exp run --queue \
-    -S model.path='Qwen/Qwen2.5-7B-Instruct' \
-    -S model.temperature='0.5' \
-    -S model.top_p='0.95' \
-    -S retriever.name='hybrid' \
-    -S retriever.top_k='1' \
-    -S run='1' \
-    -S dataset.path='bdsaglam/hotpotqa-distractor' \
-    -S dataset.name='default' \
-    -S dataset.split='"validation[:300]"' \
-    -S devices='"1"'
-
-dvc exp run --queue \
-    -S model.path='/home/baris/repos/verifiers/outputs/Llama-3.1-8B-Instruct-ragent-grpo-musique-merged' \
-    -S model.temperature='0.5' \
-    -S model.top_p='0.95' \
-    -S retriever.name='hybrid' \
-    -S retriever.top_k='1' \
-    -S run='1' \
-    -S dataset.path='bdsaglam/hotpotqa-distractor' \
-    -S dataset.name='default' \
-    -S dataset.split='"validation[:300]"' \
-    -S devices='"2"'
-
-dvc exp run --queue \
-    -S model.path='bdsaglam/Qwen2.5-1.5B-Instruct-ragent-musique' \
-    -S model.temperature='0.5' \
-    -S model.top_p='0.95' \
-    -S retriever.name='hybrid' \
-    -S retriever.top_k='1' \
-    -S run='1' \
-    -S dataset.path='bdsaglam/hotpotqa-distractor' \
-    -S dataset.name='default' \
-    -S dataset.split='"validation[:300]"' \
-    -S devices='"3"'
-
-dvc exp run --queue \
     -S model.path='meta-llama/Llama-3.1-8B-Instruct' \
     -S model.temperature='0.5' \
     -S model.top_p='0.95' \
@@ -57,6 +9,6 @@ dvc exp run --queue \
     -S run='1' \
     -S dataset.path='bdsaglam/hotpotqa-distractor' \
     -S dataset.name='default' \
-    -S dataset.split='"validation[:300]"' \
+    -S dataset.split='validation[:300]' \
     -S devices='"0"'
 
