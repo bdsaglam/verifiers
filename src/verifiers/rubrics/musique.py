9501174104440a19eb25fee4a6aaf33d90ca7756
Barış Deniz Sağlam <bdsaglam@gmail.com>
2025-04-07 00:03:43 +0300

    fix supporting metric bug

--- src/verifiers/rubrics/musique.py
+++ src/verifiers/rubrics/musique.py
@@ -40,6 +40,22 @@ def extract_all_retrieved_titles(trajectory: list[Message]) -> Generator[str, No
         yield from extract_retrieved_titles(msg["content"])
 
 
+def musique_supporting_recall_reward_func(
+    completions: List[List[Message]],
+    supporting_titles: list[list[str]],
+    **kwargs,
+) -> List[float]:
+    rewards = []
+    for completion, _supporting_titles in zip(completions, supporting_titles):
+        retrieved_titles = set(extract_all_retrieved_titles(completion))
+        if len(retrieved_titles) == 0:
+            rewards.append(0.0)
+            continue
+        recall = len(retrieved_titles & set(_supporting_titles)) / len(_supporting_titles)
+        rewards.append(recall)
+    return rewards
+
+
 def musique_supporting_f1_reward_func(
     completions: List[List[Message]],
     supporting_titles: list[list[str]],
@@ -52,7 +68,7 @@ def musique_supporting_f1_reward_func(
             rewards.append(0.0)
             continue
         precision = len(retrieved_titles & set(_supporting_titles)) / len(retrieved_titles)
-        recall = len(retrieved_titles & set(_supporting_titles)) / len(supporting_titles)
+        recall = len(retrieved_titles & set(_supporting_titles)) / len(_supporting_titles)
         f1 = 2 * precision * recall / (precision + recall) if precision + recall > 0 else 0
         rewards.append(f1)
     return rewards
