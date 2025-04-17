58829fd6a78e365e18f61123c4dc385d1dc18861
Barış Deniz Sağlam <bdsaglam@gmail.com>
2025-04-17 15:39:44 +0300

    add exp results

--- nbs/run.sh
+++ nbs/run.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 dvc exp run --queue \
-    -S model.path='bdsaglam/Qwen2.5-1.5B-Instruct-ragent-musique' \
+    -S model.path='../outputs/Llama-3.1-8B-Instruct-ragent-grpo-musique-2-merged' \
     -S model.temperature='0.5' \
     -S model.top_p='0.95' \
-    -S model.few_shot_prob='0.0' \
+    -S model.few_shot_prob='1.0' \
     -S retriever.name='hybrid' \
     -S retriever.top_k='1' \
     -S retriever.mode='new' \
@@ -18,20 +18,6 @@ dvc exp run --queue \
     -S model.path='../outputs/Llama-3.1-8B-Instruct-ragent-grpo-musique-2-merged' \
     -S model.temperature='0.5' \
     -S model.top_p='0.95' \
-    -S model.few_shot_prob='1.0' \
-    -S retriever.name='hybrid' \
-    -S retriever.top_k='1' \
-    -S retriever.mode='new' \
-    -S run='eff-ret-1' \
-    -S dataset.path='bdsaglam/musique-mini' \
-    -S dataset.name='answerable' \
-    -S dataset.split='validation' \
-    -S devices='"1"'
-
-dvc exp run --queue \
-    -S model.path='../outputs/Llama-3.1-8B-Instruct-ragent-grpo-musique-2-merged' \
-    -S model.temperature='0.5' \
-    -S model.top_p='0.95' \
     -S model.few_shot_prob='0.0' \
     -S retriever.name='hybrid' \
     -S retriever.top_k='1' \
@@ -40,5 +26,5 @@ dvc exp run --queue \
     -S dataset.path='bdsaglam/musique-mini' \
     -S dataset.name='answerable' \
     -S dataset.split='validation' \
-    -S devices='"2"'
+    -S devices='"0"'
 
