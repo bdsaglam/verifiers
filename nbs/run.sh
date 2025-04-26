093f32b01b7848afe39242ff373aa509726bf988
Barış Deniz Sağlam <bdsaglam@gmail.com>
2025-04-26 13:50:20 +0300

    prepare

--- nbs/run.sh
+++ nbs/run.sh
@@ -2,48 +2,6 @@
 
 dvc exp run --queue \
     -S model.path='Qwen/Qwen2.5-14B-Instruct' \
-    -S model.temperature='0.3' \
-    -S model.top_p='0.95' \
-    -S model.few_shot_prob='0.0' \
-    -S retriever.name='hybrid-tei' \
-    -S retriever.top_k='1' \
-    -S retriever.mode='all' \
-    -S run='1' \
-    -S dataset.path='bdsaglam/musique-mini' \
-    -S dataset.name='answerable' \
-    -S dataset.split='validation' \
-    -S devices='"0,1"'
-
-dvc exp run --queue \
-    -S model.path='Qwen/Qwen2.5-32B-Instruct' \
-    -S model.temperature='0.3' \
-    -S model.top_p='0.95' \
-    -S model.few_shot_prob='1.0' \
-    -S retriever.name='hybrid-tei' \
-    -S retriever.top_k='2' \
-    -S retriever.mode='all' \
-    -S run='1' \
-    -S dataset.path='bdsaglam/musique-mini' \
-    -S dataset.name='answerable' \
-    -S dataset.split='validation' \
-    -S devices='"2,3"'
-
-dvc exp run --queue \
-    -S model.path='Qwen/Qwen2.5-Coder-32B-Instruct' \
-    -S model.temperature='0.5' \
-    -S model.top_p='0.95' \
-    -S model.few_shot_prob='0.0' \
-    -S retriever.name='hybrid-tei' \
-    -S retriever.top_k='2' \
-    -S retriever.mode='all' \
-    -S run='1' \
-    -S dataset.path='bdsaglam/musique-mini' \
-    -S dataset.name='answerable' \
-    -S dataset.split='validation' \
-    -S devices='"0,1"'
-
-dvc exp run --queue \
-    -S model.path='Qwen/Qwen2.5-32B-Instruct' \
     -S model.temperature='0.7' \
     -S model.top_p='0.95' \
     -S model.few_shot_prob='1.0' \
@@ -54,165 +12,11 @@ dvc exp run --queue \
     -S dataset.path='bdsaglam/musique-mini' \
     -S dataset.name='answerable' \
     -S dataset.split='validation' \
-    -S devices='"2,3"'
-
-dvc exp run --queue \
-    -S model.path='Qwen/Qwen2.5-32B-Instruct' \
-    -S model.temperature='0.5' \
-    -S model.top_p='0.95' \
-    -S model.few_shot_prob='1.0' \
-    -S retriever.name='hybrid-tei' \
-    -S retriever.top_k='2' \
-    -S retriever.mode='all' \
-    -S run='1' \
-    -S dataset.path='bdsaglam/musique-mini' \
-    -S dataset.name='answerable' \
-    -S dataset.split='validation' \
     -S devices='"0,1"'
 
 dvc exp run --queue \
     -S model.path='Qwen/Qwen2.5-Coder-32B-Instruct' \
     -S model.temperature='0.7' \
-    -S model.top_p='0.95' \
-    -S model.few_shot_prob='1.0' \
-    -S retriever.name='hybrid-tei' \
-    -S retriever.top_k='2' \
-    -S retriever.mode='all' \
-    -S run='1' \
-    -S dataset.path='bdsaglam/musique-mini' \
-    -S dataset.name='answerable' \
-    -S dataset.split='validation' \
-    -S devices='"2,3"'
-
-dvc exp run --queue \
-    -S model.path='Qwen/Qwen2.5-Coder-32B-Instruct' \
-    -S model.temperature='0.7' \
-    -S model.top_p='0.95' \
-    -S model.few_shot_prob='0.0' \
-    -S retriever.name='hybrid-tei' \
-    -S retriever.top_k='2' \
-    -S retriever.mode='all' \
-    -S run='1' \
-    -S dataset.path='bdsaglam/musique-mini' \
-    -S dataset.name='answerable' \
-    -S dataset.split='validation' \
-    -S devices='"0,1"'
-
-dvc exp run --queue \
-    -S model.path='Qwen/Qwen2.5-Coder-32B-Instruct' \
-    -S model.temperature='0.7' \
-    -S model.top_p='0.95' \
-    -S model.few_shot_prob='1.0' \
-    -S retriever.name='hybrid-tei' \
-    -S retriever.top_k='3' \
-    -S retriever.mode='all' \
-    -S run='1' \
-    -S dataset.path='bdsaglam/musique-mini' \
-    -S dataset.name='answerable' \
-    -S dataset.split='validation' \
-    -S devices='"2,3"'
-
-dvc exp run --queue \
-    -S model.path='Qwen/Qwen2.5-Coder-32B-Instruct' \
-    -S model.temperature='0.3' \
-    -S model.top_p='0.95' \
-    -S model.few_shot_prob='1.0' \
-    -S retriever.name='hybrid-tei' \
-    -S retriever.top_k='1' \
-    -S retriever.mode='all' \
-    -S run='1' \
-    -S dataset.path='bdsaglam/musique-mini' \
-    -S dataset.name='answerable' \
-    -S dataset.split='validation' \
-    -S devices='"0,1"'
-
-dvc exp run --queue \
-    -S model.path='Qwen/Qwen2.5-14B-Instruct' \
-    -S model.temperature='0.7' \
-    -S model.top_p='0.95' \
-    -S model.few_shot_prob='1.0' \
-    -S retriever.name='hybrid-tei' \
-    -S retriever.top_k='1' \
-    -S retriever.mode='all' \
-    -S run='1' \
-    -S dataset.path='bdsaglam/musique-mini' \
-    -S dataset.name='answerable' \
-    -S dataset.split='validation' \
-    -S devices='"2,3"'
-
-dvc exp run --queue \
-    -S model.path='Qwen/Qwen2.5-Coder-32B-Instruct' \
-    -S model.temperature='0.7' \
-    -S model.top_p='0.95' \
-    -S model.few_shot_prob='0.0' \
-    -S retriever.name='hybrid-tei' \
-    -S retriever.top_k='3' \
-    -S retriever.mode='all' \
-    -S run='1' \
-    -S dataset.path='bdsaglam/musique-mini' \
-    -S dataset.name='answerable' \
-    -S dataset.split='validation' \
-    -S devices='"0,1"'
-
-dvc exp run --queue \
-    -S model.path='Qwen/Qwen2.5-14B-Instruct' \
-    -S model.temperature='0.3' \
-    -S model.top_p='0.95' \
-    -S model.few_shot_prob='1.0' \
-    -S retriever.name='hybrid-tei' \
-    -S retriever.top_k='2' \
-    -S retriever.mode='all' \
-    -S run='1' \
-    -S dataset.path='bdsaglam/musique-mini' \
-    -S dataset.name='answerable' \
-    -S dataset.split='validation' \
-    -S devices='"2,3"'
-
-dvc exp run --queue \
-    -S model.path='Qwen/Qwen2.5-Coder-32B-Instruct' \
-    -S model.temperature='0.5' \
-    -S model.top_p='0.95' \
-    -S model.few_shot_prob='0.0' \
-    -S retriever.name='hybrid-tei' \
-    -S retriever.top_k='1' \
-    -S retriever.mode='all' \
-    -S run='1' \
-    -S dataset.path='bdsaglam/musique-mini' \
-    -S dataset.name='answerable' \
-    -S dataset.split='validation' \
-    -S devices='"0,1"'
-
-dvc exp run --queue \
-    -S model.path='Qwen/Qwen2.5-32B-Instruct' \
-    -S model.temperature='0.5' \
-    -S model.top_p='0.95' \
-    -S model.few_shot_prob='0.0' \
-    -S retriever.name='hybrid-tei' \
-    -S retriever.top_k='2' \
-    -S retriever.mode='all' \
-    -S run='1' \
-    -S dataset.path='bdsaglam/musique-mini' \
-    -S dataset.name='answerable' \
-    -S dataset.split='validation' \
-    -S devices='"2,3"'
-
-dvc exp run --queue \
-    -S model.path='Qwen/Qwen2.5-Coder-32B-Instruct' \
-    -S model.temperature='0.5' \
-    -S model.top_p='0.95' \
-    -S model.few_shot_prob='1.0' \
-    -S retriever.name='hybrid-tei' \
-    -S retriever.top_k='2' \
-    -S retriever.mode='all' \
-    -S run='1' \
-    -S dataset.path='bdsaglam/musique-mini' \
-    -S dataset.name='answerable' \
-    -S dataset.split='validation' \
-    -S devices='"0,1"'
-
-dvc exp run --queue \
-    -S model.path='Qwen/Qwen2.5-32B-Instruct' \
-    -S model.temperature='0.5' \
     -S model.top_p='0.95' \
     -S model.few_shot_prob='0.0' \
     -S retriever.name='hybrid-tei' \
@@ -228,7 +32,7 @@ dvc exp run --queue \
     -S model.path='Qwen/Qwen2.5-14B-Instruct' \
     -S model.temperature='0.3' \
     -S model.top_p='0.95' \
-    -S model.few_shot_prob='0.0' \
+    -S model.few_shot_prob='1.0' \
     -S retriever.name='hybrid-tei' \
     -S retriever.top_k='2' \
     -S retriever.mode='all' \
@@ -253,12 +57,40 @@ dvc exp run --queue \
     -S devices='"2,3"'
 
 dvc exp run --queue \
-    -S model.path='Qwen/Qwen2.5-32B-Instruct' \
+    -S model.path='Qwen/Qwen2.5-14B-Instruct' \
     -S model.temperature='0.3' \
     -S model.top_p='0.95' \
     -S model.few_shot_prob='0.0' \
     -S retriever.name='hybrid-tei' \
-    -S retriever.top_k='2' \
+    -S retriever.top_k='1' \
+    -S retriever.mode='all' \
+    -S run='1' \
+    -S dataset.path='bdsaglam/musique-mini' \
+    -S dataset.name='answerable' \
+    -S dataset.split='validation' \
+    -S devices='"0,1"'
+
+dvc exp run --queue \
+    -S model.path='Qwen/Qwen2.5-Coder-32B-Instruct' \
+    -S model.temperature='0.3' \
+    -S model.top_p='0.95' \
+    -S model.few_shot_prob='1.0' \
+    -S retriever.name='hybrid-tei' \
+    -S retriever.top_k='1' \
+    -S retriever.mode='all' \
+    -S run='1' \
+    -S dataset.path='bdsaglam/musique-mini' \
+    -S dataset.name='answerable' \
+    -S dataset.split='validation' \
+    -S devices='"2,3"'
+
+dvc exp run --queue \
+    -S model.path='Qwen/Qwen2.5-Coder-32B-Instruct' \
+    -S model.temperature='0.5' \
+    -S model.top_p='0.95' \
+    -S model.few_shot_prob='0.0' \
+    -S retriever.name='hybrid-tei' \
+    -S retriever.top_k='1' \
     -S retriever.mode='all' \
     -S run='1' \
     -S dataset.path='bdsaglam/musique-mini' \
@@ -279,4 +111,130 @@ dvc exp run --queue \
     -S dataset.name='answerable' \
     -S dataset.split='validation' \
     -S devices='"2,3"'
+
+dvc exp run --queue \
+    -S model.path='Qwen/Qwen2.5-Coder-32B-Instruct' \
+    -S model.temperature='0.5' \
+    -S model.top_p='0.95' \
+    -S model.few_shot_prob='1.0' \
+    -S retriever.name='hybrid-tei' \
+    -S retriever.top_k='2' \
+    -S retriever.mode='all' \
+    -S run='1' \
+    -S dataset.path='bdsaglam/musique-mini' \
+    -S dataset.name='answerable' \
+    -S dataset.split='validation' \
+    -S devices='"0,1"'
+
+dvc exp run --queue \
+    -S model.path='Qwen/Qwen2.5-Coder-32B-Instruct' \
+    -S model.temperature='0.5' \
+    -S model.top_p='0.95' \
+    -S model.few_shot_prob='0.0' \
+    -S retriever.name='hybrid-tei' \
+    -S retriever.top_k='2' \
+    -S retriever.mode='all' \
+    -S run='1' \
+    -S dataset.path='bdsaglam/musique-mini' \
+    -S dataset.name='answerable' \
+    -S dataset.split='validation' \
+    -S devices='"2,3"'
+
+dvc exp run --queue \
+    -S model.path='Qwen/Qwen2.5-32B-Instruct' \
+    -S model.temperature='0.3' \
+    -S model.top_p='0.95' \
+    -S model.few_shot_prob='0.0' \
+    -S retriever.name='hybrid-tei' \
+    -S retriever.top_k='2' \
+    -S retriever.mode='all' \
+    -S run='1' \
+    -S dataset.path='bdsaglam/musique-mini' \
+    -S dataset.name='answerable' \
+    -S dataset.split='validation' \
+    -S devices='"0,1"'
+
+dvc exp run --queue \
+    -S model.path='Qwen/Qwen2.5-14B-Instruct' \
+    -S model.temperature='0.3' \
+    -S model.top_p='0.95' \
+    -S model.few_shot_prob='0.0' \
+    -S retriever.name='hybrid-tei' \
+    -S retriever.top_k='2' \
+    -S retriever.mode='all' \
+    -S run='1' \
+    -S dataset.path='bdsaglam/musique-mini' \
+    -S dataset.name='answerable' \
+    -S dataset.split='validation' \
+    -S devices='"2,3"'
+
+dvc exp run --queue \
+    -S model.path='Qwen/Qwen2.5-Coder-32B-Instruct' \
+    -S model.temperature='0.7' \
+    -S model.top_p='0.95' \
+    -S model.few_shot_prob='1.0' \
+    -S retriever.name='hybrid-tei' \
+    -S retriever.top_k='2' \
+    -S retriever.mode='all' \
+    -S run='1' \
+    -S dataset.path='bdsaglam/musique-mini' \
+    -S dataset.name='answerable' \
+    -S dataset.split='validation' \
+    -S devices='"0,1"'
+
+dvc exp run --queue \
+    -S model.path='Qwen/Qwen2.5-Coder-32B-Instruct' \
+    -S model.temperature='0.7' \
+    -S model.top_p='0.95' \
+    -S model.few_shot_prob='0.0' \
+    -S retriever.name='hybrid-tei' \
+    -S retriever.top_k='2' \
+    -S retriever.mode='all' \
+    -S run='1' \
+    -S dataset.path='bdsaglam/musique-mini' \
+    -S dataset.name='answerable' \
+    -S dataset.split='validation' \
+    -S devices='"2,3"'
+
+dvc exp run --queue \
+    -S model.path='Qwen/Qwen2.5-32B-Instruct' \
+    -S model.temperature='0.5' \
+    -S model.top_p='0.95' \
+    -S model.few_shot_prob='0.0' \
+    -S retriever.name='hybrid-tei' \
+    -S retriever.top_k='3' \
+    -S retriever.mode='all' \
+    -S run='1' \
+    -S dataset.path='bdsaglam/musique-mini' \
+    -S dataset.name='answerable' \
+    -S dataset.split='validation' \
+    -S devices='"0,1"'
+
+dvc exp run --queue \
+    -S model.path='Qwen/Qwen2.5-32B-Instruct' \
+    -S model.temperature='0.5' \
+    -S model.top_p='0.95' \
+    -S model.few_shot_prob='0.0' \
+    -S retriever.name='hybrid-tei' \
+    -S retriever.top_k='2' \
+    -S retriever.mode='all' \
+    -S run='1' \
+    -S dataset.path='bdsaglam/musique-mini' \
+    -S dataset.name='answerable' \
+    -S dataset.split='validation' \
+    -S devices='"2,3"'
+
+dvc exp run --queue \
+    -S model.path='Qwen/Qwen2.5-Coder-32B-Instruct' \
+    -S model.temperature='0.7' \
+    -S model.top_p='0.95' \
+    -S model.few_shot_prob='1.0' \
+    -S retriever.name='hybrid-tei' \
+    -S retriever.top_k='3' \
+    -S retriever.mode='all' \
+    -S run='1' \
+    -S dataset.path='bdsaglam/musique-mini' \
+    -S dataset.name='answerable' \
+    -S dataset.split='validation' \
+    -S devices='"0,1"'
 
--- scripts/ragent.py
+++ scripts/ragent.py
@@ -321,7 +321,7 @@ def predict(
         model=model_path,
         trust_remote_code=True,
         dtype=get_half_precision_dtype(),
-        gpu_memory_utilization=0.90,
+        gpu_memory_utilization=0.70,
         tensor_parallel_size=os.getenv("CUDA_VISIBLE_DEVICES", "0").count(",") + 1,
         seed=seed,
         max_model_len=8192*2,
