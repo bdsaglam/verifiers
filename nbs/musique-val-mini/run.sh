10f57312dee9b0ec97c6782c518fe7501cc1709a
Barış Deniz Sağlam <bdsaglam@gmail.com>
2025-05-23 17:59:35 +0300

    prepare

--- nbs/musique-val-mini/run.sh
+++ nbs/musique-val-mini/run.sh
@@ -1,2 +1,58 @@
 #!/bin/sh
 
+dvc exp run --queue \
+    -S model.path='bdsaglam/Qwen2.5-1.5B-Instruct-ragent-musique' \
+    -S model.temperature='0.5' \
+    -S model.top_p='0.95' \
+    -S model.few_shot_prob='0.0' \
+    -S retriever.name='hybrid-tei' \
+    -S retriever.top_k='1' \
+    -S repeat='5' \
+    -S run='1' \
+    -S dataset.path='bdsaglam/musique-mini' \
+    -S dataset.name='answerable' \
+    -S dataset.split='validation' \
+    -S devices='"1"'
+
+dvc exp run --queue \
+    -S model.path='meta-llama/Llama-3.1-8B-Instruct' \
+    -S model.temperature='0.5' \
+    -S model.top_p='0.95' \
+    -S model.few_shot_prob='0.0' \
+    -S retriever.name='hybrid-tei' \
+    -S retriever.top_k='1' \
+    -S repeat='5' \
+    -S run='1' \
+    -S dataset.path='bdsaglam/musique-mini' \
+    -S dataset.name='answerable' \
+    -S dataset.split='validation' \
+    -S devices='"1"'
+
+dvc exp run --queue \
+    -S model.path='/home/baris/repos/verifiers/outputs/Llama-3.1-8B-Instruct-ragent-grpo-20250520_080809-merged' \
+    -S model.temperature='0.5' \
+    -S model.top_p='0.95' \
+    -S model.few_shot_prob='0.0' \
+    -S retriever.name='hybrid-tei' \
+    -S retriever.top_k='1' \
+    -S repeat='5' \
+    -S run='1' \
+    -S dataset.path='bdsaglam/musique-mini' \
+    -S dataset.name='answerable' \
+    -S dataset.split='validation' \
+    -S devices='"1"'
+
+dvc exp run --queue \
+    -S model.path='bdsaglam/Llama-3.1-8B-Instruct-ragent-grpo-musique-merged' \
+    -S model.temperature='0.5' \
+    -S model.top_p='0.95' \
+    -S model.few_shot_prob='0.0' \
+    -S retriever.name='hybrid-tei' \
+    -S retriever.top_k='1' \
+    -S repeat='5' \
+    -S run='1' \
+    -S dataset.path='bdsaglam/musique-mini' \
+    -S dataset.name='answerable' \
+    -S dataset.split='validation' \
+    -S devices='"1"'
+
--- nbs/musique-val/run.sh
+++ nbs/musique-val/run.sh
@@ -12,20 +12,6 @@ dvc exp run --queue \
     -S dataset.path='bdsaglam/musique' \
     -S dataset.name='answerable' \
     -S dataset.split='validation' \
-    -S devices='"1"'
-
-dvc exp run --queue \
-    -S model.path='bdsaglam/Llama-3.1-8B-Instruct-ragent-grpo-musique-merged' \
-    -S model.temperature='0.5' \
-    -S model.top_p='0.95' \
-    -S model.few_shot_prob='0.0' \
-    -S retriever.name='hybrid-tei' \
-    -S retriever.top_k='1' \
-    -S repeat='5' \
-    -S run='1' \
-    -S dataset.path='bdsaglam/musique' \
-    -S dataset.name='answerable' \
-    -S dataset.split='validation' \
     -S devices='"2"'
 
 dvc exp run --queue \
@@ -54,10 +40,24 @@ dvc exp run --queue \
     -S dataset.path='bdsaglam/musique' \
     -S dataset.name='answerable' \
     -S dataset.split='validation' \
-    -S devices='"1"'
+    -S devices='"2"'
 
 dvc exp run --queue \
     -S model.path='/home/baris/repos/verifiers/outputs/Llama-3.1-8B-Instruct-ragent-grpo-20250508_213215-merged' \
+    -S model.temperature='0.5' \
+    -S model.top_p='0.95' \
+    -S model.few_shot_prob='0.0' \
+    -S retriever.name='hybrid-tei' \
+    -S retriever.top_k='1' \
+    -S repeat='5' \
+    -S run='1' \
+    -S dataset.path='bdsaglam/musique' \
+    -S dataset.name='answerable' \
+    -S dataset.split='validation' \
+    -S devices='"3"'
+
+dvc exp run --queue \
+    -S model.path='bdsaglam/Llama-3.1-8B-Instruct-ragent-grpo-musique-merged' \
     -S model.temperature='0.5' \
     -S model.top_p='0.95' \
     -S model.few_shot_prob='0.0' \
