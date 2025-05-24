76b953355f967f862729d77b94b4dfaaf2e23132
Barış Deniz Sağlam <bdsaglam@gmail.com>
2025-05-24 11:59:06 +0300

    add more results

--- nbs/musique-val/run.sh
+++ nbs/musique-val/run.sh
@@ -1,17 +1,2 @@
 #!/bin/sh
 
-dvc exp run --queue \
-    -S model.path='meta-llama/Llama-3.1-8B-Instruct' \
-    -S model.temperature='0.5' \
-    -S model.top_p='0.95' \
-    -S model.few_shot_prob='0.0' \
-    -S retriever.name='hybrid-tei' \
-    -S retriever.top_k='1' \
-    -S repeat='5' \
-    -S run='1' \
-    -S dataset.path='bdsaglam/musique' \
-    -S dataset.name='answerable' \
-    -S dataset.split='validation' \
-    -S devices='"0,1,2,3"'
-
-
