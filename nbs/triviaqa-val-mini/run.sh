61106030ae98d62fe07d55b5020d28f0f7b97481
Barış Deniz Sağlam <bdsaglam@gmail.com>
2025-05-30 12:53:45 +0300

    more results

--- nbs/triviaqa-val-mini/run.sh
+++ nbs/triviaqa-val-mini/run.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
 
 dvc exp run --queue \
-    -S model.path='bdsaglam/Llama-3.1-8B-Instruct-ragent-grpo-20250526_110630' \
+    -S model.path='Qwen/Qwen2.5-7B-Instruct' \
     -S model.temperature='0.5' \
     -S model.top_p='0.95' \
     -S model.few_shot_prob='0.0' \
     -S retriever.name='hybrid-tei' \
     -S retriever.top_k='1' \
-    -S repeat='5' \
+    -S repeat='1' \
     -S run='1' \
     -S dataset.path='bdsaglam/triviaqa-wiki-musique-mini' \
     -S dataset.name='default' \
     -S dataset.split='validation' \
-    -S devices='"0,1,2,3"'
+    -S devices='"0"'
 
 dvc exp run --queue \
     -S model.path='/home/baris/repos/verifiers/outputs/Llama-3.1-8B-Instruct-ragent-grpo-20250520_080809-merged' \
@@ -26,49 +26,7 @@ dvc exp run --queue \
     -S dataset.path='bdsaglam/triviaqa-wiki-musique-mini' \
     -S dataset.name='default' \
     -S dataset.split='validation' \
-    -S devices='"0,1,2,3"'
-
-dvc exp run --queue \
-    -S model.path='meta-llama/Llama-3.1-8B-Instruct' \
-    -S model.temperature='0.5' \
-    -S model.top_p='0.95' \
-    -S model.few_shot_prob='0.0' \
-    -S retriever.name='hybrid-tei' \
-    -S retriever.top_k='1' \
-    -S repeat='1' \
-    -S run='1' \
-    -S dataset.path='bdsaglam/triviaqa-wiki-musique-mini' \
-    -S dataset.name='default' \
-    -S dataset.split='validation' \
-    -S devices='"0,1,2,3"'
-
-dvc exp run --queue \
-    -S model.path='bdsaglam/Llama-3.1-8B-Instruct-ragent-grpo-musique-merged' \
-    -S model.temperature='0.5' \
-    -S model.top_p='0.95' \
-    -S model.few_shot_prob='0.0' \
-    -S retriever.name='hybrid-tei' \
-    -S retriever.top_k='1' \
-    -S repeat='5' \
-    -S run='1' \
-    -S dataset.path='bdsaglam/triviaqa-wiki-musique-mini' \
-    -S dataset.name='default' \
-    -S dataset.split='validation' \
-    -S devices='"0,1,2,3"'
-
-dvc exp run --queue \
-    -S model.path='meta-llama/Llama-3.1-8B-Instruct' \
-    -S model.temperature='0.5' \
-    -S model.top_p='0.95' \
-    -S model.few_shot_prob='0.0' \
-    -S retriever.name='hybrid-tei' \
-    -S retriever.top_k='1' \
-    -S repeat='5' \
-    -S run='1' \
-    -S dataset.path='bdsaglam/triviaqa-wiki-musique-mini' \
-    -S dataset.name='default' \
-    -S dataset.split='validation' \
-    -S devices='"0,1,2,3"'
+    -S devices='"1"'
 
 dvc exp run --queue \
     -S model.path='bdsaglam/Llama-3.1-8B-Instruct-ragent-grpo-20250526_110630' \
@@ -82,7 +40,7 @@ dvc exp run --queue \
     -S dataset.path='bdsaglam/triviaqa-wiki-musique-mini' \
     -S dataset.name='default' \
     -S dataset.split='validation' \
-    -S devices='"0,1,2,3"'
+    -S devices='"2"'
 
 dvc exp run --queue \
     -S model.path='/home/baris/repos/verifiers/outputs/Llama-3.1-8B-Instruct-ragent-grpo-20250508_213215-merged' \
@@ -96,35 +54,7 @@ dvc exp run --queue \
     -S dataset.path='bdsaglam/triviaqa-wiki-musique-mini' \
     -S dataset.name='default' \
     -S dataset.split='validation' \
-    -S devices='"0,1,2,3"'
-
-dvc exp run --queue \
-    -S model.path='Qwen/Qwen2.5-7B-Instruct' \
-    -S model.temperature='0.5' \
-    -S model.top_p='0.95' \
-    -S model.few_shot_prob='0.0' \
-    -S retriever.name='hybrid-tei' \
-    -S retriever.top_k='1' \
-    -S repeat='5' \
-    -S run='1' \
-    -S dataset.path='bdsaglam/triviaqa-wiki-musique-mini' \
-    -S dataset.name='default' \
-    -S dataset.split='validation' \
-    -S devices='"0,1,2,3"'
-
-dvc exp run --queue \
-    -S model.path='/home/baris/repos/verifiers/outputs/Llama-3.1-8B-Instruct-ragent-grpo-20250508_213215-merged' \
-    -S model.temperature='0.5' \
-    -S model.top_p='0.95' \
-    -S model.few_shot_prob='0.0' \
-    -S retriever.name='hybrid-tei' \
-    -S retriever.top_k='1' \
-    -S repeat='5' \
-    -S run='1' \
-    -S dataset.path='bdsaglam/triviaqa-wiki-musique-mini' \
-    -S dataset.name='default' \
-    -S dataset.split='validation' \
-    -S devices='"0,1,2,3"'
+    -S devices='"3"'
 
 dvc exp run --queue \
     -S model.path='bdsaglam/Llama-3.1-8B-Instruct-ragent-grpo-musique-merged' \
@@ -138,24 +68,10 @@ dvc exp run --queue \
     -S dataset.path='bdsaglam/triviaqa-wiki-musique-mini' \
     -S dataset.name='default' \
     -S dataset.split='validation' \
-    -S devices='"0,1,2,3"'
+    -S devices='"0"'
 
 dvc exp run --queue \
-    -S model.path='/home/baris/repos/verifiers/outputs/Llama-3.1-8B-Instruct-ragent-grpo-20250520_080809-merged' \
-    -S model.temperature='0.5' \
-    -S model.top_p='0.95' \
-    -S model.few_shot_prob='0.0' \
-    -S retriever.name='hybrid-tei' \
-    -S retriever.top_k='1' \
-    -S repeat='5' \
-    -S run='1' \
-    -S dataset.path='bdsaglam/triviaqa-wiki-musique-mini' \
-    -S dataset.name='default' \
-    -S dataset.split='validation' \
-    -S devices='"0,1,2,3"'
-
-dvc exp run --queue \
-    -S model.path='Qwen/Qwen2.5-7B-Instruct' \
+    -S model.path='meta-llama/Llama-3.1-8B-Instruct' \
     -S model.temperature='0.5' \
     -S model.top_p='0.95' \
     -S model.few_shot_prob='0.0' \
@@ -166,5 +82,5 @@ dvc exp run --queue \
     -S dataset.path='bdsaglam/triviaqa-wiki-musique-mini' \
     -S dataset.name='default' \
     -S dataset.split='validation' \
-    -S devices='"0,1,2,3"'
+    -S devices='"1"'
 
