44947b823e28ea7de43dd17d0aa08e4623295cdb
Barış Deniz Sağlam <bdsaglam@gmail.com>
2025-05-30 12:13:49 +0300

    add triviqa eval

--- nbs/musique-val/run.sh
+++ nbs/musique-val/run.sh
@@ -1,7 +1,21 @@
 #!/bin/sh
 
 dvc exp run --queue \
-    -S model.path='deepseek-ai/DeepSeek-R1-0528-Qwen3-8B' \
+    -S model.path='meta-llama/Llama-3.1-70B-Instruct' \
+    -S model.temperature='0.5' \
+    -S model.top_p='0.95' \
+    -S model.few_shot_prob='0.0' \
+    -S retriever.name='hybrid-tei' \
+    -S retriever.top_k='1' \
+    -S repeat='1' \
+    -S run='1' \
+    -S dataset.path='bdsaglam/musique' \
+    -S dataset.name='answerable' \
+    -S dataset.split='validation' \
+    -S devices='"0,1,2,3"'
+
+dvc exp run --queue \
+    -S model.path='Qwen/Qwen2.5-32B-Instruct' \
     -S model.temperature='0.5' \
     -S model.top_p='0.95' \
     -S model.few_shot_prob='0.0' \
@@ -15,13 +29,27 @@ dvc exp run --queue \
     -S devices='"0,1,2,3"'
 
 dvc exp run --queue \
-    -S model.path='deepseek-ai/DeepSeek-R1-0528-Qwen3-8B' \
+    -S model.path='Qwen/Qwen2.5-32B-Instruct' \
     -S model.temperature='0.5' \
     -S model.top_p='0.95' \
     -S model.few_shot_prob='0.0' \
     -S retriever.name='hybrid-tei' \
     -S retriever.top_k='1' \
     -S repeat='1' \
+    -S run='1' \
+    -S dataset.path='bdsaglam/musique' \
+    -S dataset.name='answerable' \
+    -S dataset.split='validation' \
+    -S devices='"0,1,2,3"'
+
+dvc exp run --queue \
+    -S model.path='meta-llama/Llama-3.1-70B-Instruct' \
+    -S model.temperature='0.5' \
+    -S model.top_p='0.95' \
+    -S model.few_shot_prob='0.0' \
+    -S retriever.name='hybrid-tei' \
+    -S retriever.top_k='1' \
+    -S repeat='5' \
     -S run='1' \
     -S dataset.path='bdsaglam/musique' \
     -S dataset.name='answerable' \
