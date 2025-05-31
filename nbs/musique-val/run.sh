fcaf965dd7e86465d75678ee7b9658fd14f15e9c
Barış Deniz Sağlam <bdsaglam@gmail.com>
2025-06-01 00:45:52 +0300

    fix dockerfile

--- nbs/musique-val/run.sh
+++ nbs/musique-val/run.sh
@@ -1,30 +1,16 @@
 #!/bin/sh
 
 dvc exp run --queue \
-    -S model.path='Qwen/Qwen2.5-32B-Instruct' \
+    -S model.path='bdsaglam/Llama-3.1-8B-Instruct-ragent-grpo-20250531_141657' \
     -S model.temperature='0.5' \
     -S model.top_p='0.95' \
     -S model.few_shot_prob='0.0' \
     -S retriever.name='hybrid-tei' \
     -S retriever.top_k='1' \
-    -S repeat='5' \
+    -S repeat='1' \
     -S run='1' \
     -S dataset.path='bdsaglam/musique' \
     -S dataset.name='answerable' \
     -S dataset.split='validation' \
-    -S devices='"0,1,2,3"'
-
-dvc exp run --queue \
-    -S model.path='meta-llama/Llama-3.1-70B-Instruct' \
-    -S model.temperature='0.5' \
-    -S model.top_p='0.95' \
-    -S model.few_shot_prob='0.0' \
-    -S retriever.name='hybrid-tei' \
-    -S retriever.top_k='1' \
-    -S repeat='5' \
-    -S run='1' \
-    -S dataset.path='bdsaglam/musique' \
-    -S dataset.name='answerable' \
-    -S dataset.split='validation' \
-    -S devices='"0,1,2,3"'
+    -S devices='"2,3"'
 
