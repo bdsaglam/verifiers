1382325b266d30dd5990dcab0cdc47e3948de6f4
Barış Deniz Sağlam <bdsaglam@gmail.com>
2025-06-01 23:23:25 +0300

    prepare

--- nbs/wiki-triviaqa-val-mini/run.sh
+++ nbs/wiki-triviaqa-val-mini/run.sh
@@ -2,7 +2,7 @@
 
 dvc exp run --queue \
     -S agent='wagent' \
-    -S model.path='Qwen/Qwen2.5-7B-Instruct' \
+    -S model.path='bdsaglam/Llama-3.1-8B-Instruct-ragent-grpo-20250520_080809-merged' \
     -S model.temperature='0.5' \
     -S model.top_p='0.95' \
     -S model.few_shot_prob='0.0' \
@@ -13,37 +13,7 @@ dvc exp run --queue \
     -S dataset.path='bdsaglam/triviaqa-wiki-musique-mini' \
     -S dataset.name='default' \
     -S dataset.split='validation' \
-    -S devices='"0"'
-
-dvc exp run --queue \
-    -S agent='wagent' \
-    -S model.path='bdsaglam/Llama-3.1-8B-Instruct-ragent-grpo-musique-merged' \
-    -S model.temperature='0.5' \
-    -S model.top_p='0.95' \
-    -S model.few_shot_prob='0.0' \
-    -S retriever.name='wiki' \
-    -S retriever.top_k='1' \
-    -S repeat='1' \
-    -S run='1' \
-    -S dataset.path='bdsaglam/triviaqa-wiki-musique-mini' \
-    -S dataset.name='default' \
-    -S dataset.split='validation' \
-    -S devices='"2"'
-
-dvc exp run --queue \
-    -S agent='wagent' \
-    -S model.path='Qwen/Qwen2.5-7B-Instruct' \
-    -S model.temperature='0.5' \
-    -S model.top_p='0.95' \
-    -S model.few_shot_prob='1.0' \
-    -S retriever.name='wiki' \
-    -S retriever.top_k='1' \
-    -S repeat='1' \
-    -S run='1' \
-    -S dataset.path='bdsaglam/triviaqa-wiki-musique-mini' \
-    -S dataset.name='default' \
-    -S dataset.split='validation' \
-    -S devices='"3"'
+    -S devices='"0,1,2,3"'
 
 dvc exp run --queue \
     -S agent='wagent' \
@@ -58,37 +28,7 @@ dvc exp run --queue \
     -S dataset.path='bdsaglam/triviaqa-wiki-musique-mini' \
     -S dataset.name='default' \
     -S dataset.split='validation' \
-    -S devices='"3"'
-
-dvc exp run --queue \
-    -S agent='wagent' \
-    -S model.path='bdsaglam/Llama-3.1-8B-Instruct-ragent-grpo-20250520_080809-merged' \
-    -S model.temperature='0.5' \
-    -S model.top_p='0.95' \
-    -S model.few_shot_prob='1.0' \
-    -S retriever.name='wiki' \
-    -S retriever.top_k='1' \
-    -S repeat='1' \
-    -S run='1' \
-    -S dataset.path='bdsaglam/triviaqa-wiki-musique-mini' \
-    -S dataset.name='default' \
-    -S dataset.split='validation' \
-    -S devices='"2"'
-
-dvc exp run --queue \
-    -S agent='wagent' \
-    -S model.path='bdsaglam/Llama-3.1-8B-Instruct-ragent-grpo-musique-merged' \
-    -S model.temperature='0.5' \
-    -S model.top_p='0.95' \
-    -S model.few_shot_prob='1.0' \
-    -S retriever.name='wiki' \
-    -S retriever.top_k='1' \
-    -S repeat='1' \
-    -S run='1' \
-    -S dataset.path='bdsaglam/triviaqa-wiki-musique-mini' \
-    -S dataset.name='default' \
-    -S dataset.split='validation' \
-    -S devices='"3"'
+    -S devices='"0,1,2,3"'
 
 dvc exp run --queue \
     -S agent='wagent' \
@@ -103,11 +43,41 @@ dvc exp run --queue \
     -S dataset.path='bdsaglam/triviaqa-wiki-musique-mini' \
     -S dataset.name='default' \
     -S dataset.split='validation' \
-    -S devices='"0"'
+    -S devices='"0,1,2,3"'
 
 dvc exp run --queue \
     -S agent='wagent' \
-    -S model.path='bdsaglam/Llama-3.1-8B-Instruct-ragent-grpo-20250520_080809-merged' \
+    -S model.path='Qwen/Qwen2.5-7B-Instruct' \
+    -S model.temperature='0.5' \
+    -S model.top_p='0.95' \
+    -S model.few_shot_prob='1.0' \
+    -S retriever.name='wiki' \
+    -S retriever.top_k='1' \
+    -S repeat='1' \
+    -S run='1' \
+    -S dataset.path='bdsaglam/triviaqa-wiki-musique-mini' \
+    -S dataset.name='default' \
+    -S dataset.split='validation' \
+    -S devices='"0,1,2,3"'
+
+dvc exp run --queue \
+    -S agent='wagent' \
+    -S model.path='bdsaglam/Llama-3.1-8B-Instruct-ragent-grpo-musique-merged' \
+    -S model.temperature='0.0' \
+    -S model.top_p='0.95' \
+    -S model.few_shot_prob='1.0' \
+    -S retriever.name='wiki' \
+    -S retriever.top_k='1' \
+    -S repeat='1' \
+    -S run='1' \
+    -S dataset.path='bdsaglam/triviaqa-wiki-musique-mini' \
+    -S dataset.name='default' \
+    -S dataset.split='validation' \
+    -S devices='"0,1,2,3"'
+
+dvc exp run --queue \
+    -S agent='wagent' \
+    -S model.path='Qwen/Qwen2.5-14B-Instruct' \
     -S model.temperature='0.5' \
     -S model.top_p='0.95' \
     -S model.few_shot_prob='0.0' \
@@ -118,5 +88,215 @@ dvc exp run --queue \
     -S dataset.path='bdsaglam/triviaqa-wiki-musique-mini' \
     -S dataset.name='default' \
     -S dataset.split='validation' \
-    -S devices='"2"'
+    -S devices='"0,1,2,3"'
+
+dvc exp run --queue \
+    -S agent='wagent' \
+    -S model.path='Qwen/Qwen2.5-14B-Instruct' \
+    -S model.temperature='0.5' \
+    -S model.top_p='0.95' \
+    -S model.few_shot_prob='1.0' \
+    -S retriever.name='wiki' \
+    -S retriever.top_k='1' \
+    -S repeat='1' \
+    -S run='1' \
+    -S dataset.path='bdsaglam/triviaqa-wiki-musique-mini' \
+    -S dataset.name='default' \
+    -S dataset.split='validation' \
+    -S devices='"0,1,2,3"'
+
+dvc exp run --queue \
+    -S agent='wagent' \
+    -S model.path='bdsaglam/Llama-3.1-8B-Instruct-ragent-grpo-20250520_080809-merged' \
+    -S model.temperature='0.5' \
+    -S model.top_p='0.95' \
+    -S model.few_shot_prob='1.0' \
+    -S retriever.name='wiki' \
+    -S retriever.top_k='1' \
+    -S repeat='1' \
+    -S run='1' \
+    -S dataset.path='bdsaglam/triviaqa-wiki-musique-mini' \
+    -S dataset.name='default' \
+    -S dataset.split='validation' \
+    -S devices='"0,1,2,3"'
+
+dvc exp run --queue \
+    -S agent='wagent' \
+    -S model.path='bdsaglam/Llama-3.1-8B-Instruct-ragent-grpo-20250520_080809-merged' \
+    -S model.temperature='0.0' \
+    -S model.top_p='0.95' \
+    -S model.few_shot_prob='1.0' \
+    -S retriever.name='wiki' \
+    -S retriever.top_k='1' \
+    -S repeat='1' \
+    -S run='1' \
+    -S dataset.path='bdsaglam/triviaqa-wiki-musique-mini' \
+    -S dataset.name='default' \
+    -S dataset.split='validation' \
+    -S devices='"0,1,2,3"'
+
+dvc exp run --queue \
+    -S agent='wagent' \
+    -S model.path='bdsaglam/Llama-3.1-8B-Instruct-ragent-grpo-musique-merged' \
+    -S model.temperature='0.5' \
+    -S model.top_p='0.95' \
+    -S model.few_shot_prob='0.0' \
+    -S retriever.name='wiki' \
+    -S retriever.top_k='1' \
+    -S repeat='1' \
+    -S run='1' \
+    -S dataset.path='bdsaglam/triviaqa-wiki-musique-mini' \
+    -S dataset.name='default' \
+    -S dataset.split='validation' \
+    -S devices='"0,1,2,3"'
+
+dvc exp run --queue \
+    -S agent='wagent' \
+    -S model.path='meta-llama/Llama-3.1-8B-Instruct' \
+    -S model.temperature='0.0' \
+    -S model.top_p='0.95' \
+    -S model.few_shot_prob='0.0' \
+    -S retriever.name='wiki' \
+    -S retriever.top_k='1' \
+    -S repeat='1' \
+    -S run='1' \
+    -S dataset.path='bdsaglam/triviaqa-wiki-musique-mini' \
+    -S dataset.name='default' \
+    -S dataset.split='validation' \
+    -S devices='"0,1,2,3"'
+
+dvc exp run --queue \
+    -S agent='wagent' \
+    -S model.path='meta-llama/Llama-3.1-8B-Instruct' \
+    -S model.temperature='0.0' \
+    -S model.top_p='0.95' \
+    -S model.few_shot_prob='1.0' \
+    -S retriever.name='wiki' \
+    -S retriever.top_k='1' \
+    -S repeat='1' \
+    -S run='1' \
+    -S dataset.path='bdsaglam/triviaqa-wiki-musique-mini' \
+    -S dataset.name='default' \
+    -S dataset.split='validation' \
+    -S devices='"0,1,2,3"'
+
+dvc exp run --queue \
+    -S agent='wagent' \
+    -S model.path='Qwen/Qwen2.5-7B-Instruct' \
+    -S model.temperature='0.0' \
+    -S model.top_p='0.95' \
+    -S model.few_shot_prob='0.0' \
+    -S retriever.name='wiki' \
+    -S retriever.top_k='1' \
+    -S repeat='1' \
+    -S run='1' \
+    -S dataset.path='bdsaglam/triviaqa-wiki-musique-mini' \
+    -S dataset.name='default' \
+    -S dataset.split='validation' \
+    -S devices='"0,1,2,3"'
+
+dvc exp run --queue \
+    -S agent='wagent' \
+    -S model.path='bdsaglam/Llama-3.1-8B-Instruct-ragent-grpo-musique-merged' \
+    -S model.temperature='0.0' \
+    -S model.top_p='0.95' \
+    -S model.few_shot_prob='0.0' \
+    -S retriever.name='wiki' \
+    -S retriever.top_k='1' \
+    -S repeat='1' \
+    -S run='1' \
+    -S dataset.path='bdsaglam/triviaqa-wiki-musique-mini' \
+    -S dataset.name='default' \
+    -S dataset.split='validation' \
+    -S devices='"0,1,2,3"'
+
+dvc exp run --queue \
+    -S agent='wagent' \
+    -S model.path='Qwen/Qwen2.5-14B-Instruct' \
+    -S model.temperature='0.0' \
+    -S model.top_p='0.95' \
+    -S model.few_shot_prob='0.0' \
+    -S retriever.name='wiki' \
+    -S retriever.top_k='1' \
+    -S repeat='1' \
+    -S run='1' \
+    -S dataset.path='bdsaglam/triviaqa-wiki-musique-mini' \
+    -S dataset.name='default' \
+    -S dataset.split='validation' \
+    -S devices='"0,1,2,3"'
+
+dvc exp run --queue \
+    -S agent='wagent' \
+    -S model.path='Qwen/Qwen2.5-7B-Instruct' \
+    -S model.temperature='0.0' \
+    -S model.top_p='0.95' \
+    -S model.few_shot_prob='1.0' \
+    -S retriever.name='wiki' \
+    -S retriever.top_k='1' \
+    -S repeat='1' \
+    -S run='1' \
+    -S dataset.path='bdsaglam/triviaqa-wiki-musique-mini' \
+    -S dataset.name='default' \
+    -S dataset.split='validation' \
+    -S devices='"0,1,2,3"'
+
+dvc exp run --queue \
+    -S agent='wagent' \
+    -S model.path='bdsaglam/Llama-3.1-8B-Instruct-ragent-grpo-musique-merged' \
+    -S model.temperature='0.5' \
+    -S model.top_p='0.95' \
+    -S model.few_shot_prob='1.0' \
+    -S retriever.name='wiki' \
+    -S retriever.top_k='1' \
+    -S repeat='1' \
+    -S run='1' \
+    -S dataset.path='bdsaglam/triviaqa-wiki-musique-mini' \
+    -S dataset.name='default' \
+    -S dataset.split='validation' \
+    -S devices='"0,1,2,3"'
+
+dvc exp run --queue \
+    -S agent='wagent' \
+    -S model.path='Qwen/Qwen2.5-7B-Instruct' \
+    -S model.temperature='0.5' \
+    -S model.top_p='0.95' \
+    -S model.few_shot_prob='0.0' \
+    -S retriever.name='wiki' \
+    -S retriever.top_k='1' \
+    -S repeat='1' \
+    -S run='1' \
+    -S dataset.path='bdsaglam/triviaqa-wiki-musique-mini' \
+    -S dataset.name='default' \
+    -S dataset.split='validation' \
+    -S devices='"0,1,2,3"'
+
+dvc exp run --queue \
+    -S agent='wagent' \
+    -S model.path='Qwen/Qwen2.5-14B-Instruct' \
+    -S model.temperature='0.0' \
+    -S model.top_p='0.95' \
+    -S model.few_shot_prob='1.0' \
+    -S retriever.name='wiki' \
+    -S retriever.top_k='1' \
+    -S repeat='1' \
+    -S run='1' \
+    -S dataset.path='bdsaglam/triviaqa-wiki-musique-mini' \
+    -S dataset.name='default' \
+    -S dataset.split='validation' \
+    -S devices='"0,1,2,3"'
+
+dvc exp run --queue \
+    -S agent='wagent' \
+    -S model.path='bdsaglam/Llama-3.1-8B-Instruct-ragent-grpo-20250520_080809-merged' \
+    -S model.temperature='0.0' \
+    -S model.top_p='0.95' \
+    -S model.few_shot_prob='0.0' \
+    -S retriever.name='wiki' \
+    -S retriever.top_k='1' \
+    -S repeat='1' \
+    -S run='1' \
+    -S dataset.path='bdsaglam/triviaqa-wiki-musique-mini' \
+    -S dataset.name='default' \
+    -S dataset.split='validation' \
+    -S devices='"0,1,2,3"'
 
