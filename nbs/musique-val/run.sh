67d06de40ddceed823ac7a07cd17acf58315dace
Barış Deniz Sağlam <bdsaglam@gmail.com>
2025-06-01 10:55:44 +0300

    prepare

--- nbs/musique-val/run.sh
+++ nbs/musique-val/run.sh
@@ -1,6 +1,67 @@
 #!/bin/sh
 
 dvc exp run --queue \
+    -S agent='ragent' \
+    -S model.path='bdsaglam/Llama-3.1-8B-Instruct-ragent-grpo-20250520_080809-merged' \
+    -S model.temperature='0.5' \
+    -S model.top_p='0.95' \
+    -S model.few_shot_prob='1.0' \
+    -S retriever.name='hybrid-tei' \
+    -S retriever.top_k='1' \
+    -S repeat='1' \
+    -S run='1' \
+    -S dataset.path='bdsaglam/musique' \
+    -S dataset.name='answerable' \
+    -S dataset.split='validation' \
+    -S devices='"3"'
+
+dvc exp run --queue \
+    -S agent='ragent' \
+    -S model.path='bdsaglam/Llama-3.1-8B-Instruct-ragent-grpo-20250526_110630' \
+    -S model.temperature='0.5' \
+    -S model.top_p='0.95' \
+    -S model.few_shot_prob='1.0' \
+    -S retriever.name='hybrid-tei' \
+    -S retriever.top_k='1' \
+    -S repeat='1' \
+    -S run='1' \
+    -S dataset.path='bdsaglam/musique' \
+    -S dataset.name='answerable' \
+    -S dataset.split='validation' \
+    -S devices='"3"'
+
+dvc exp run --queue \
+    -S agent='ragent' \
+    -S model.path='meta-llama/Llama-3.1-8B-Instruct' \
+    -S model.temperature='0.5' \
+    -S model.top_p='0.95' \
+    -S model.few_shot_prob='1.0' \
+    -S retriever.name='hybrid-tei' \
+    -S retriever.top_k='1' \
+    -S repeat='5' \
+    -S run='1' \
+    -S dataset.path='bdsaglam/musique' \
+    -S dataset.name='answerable' \
+    -S dataset.split='validation' \
+    -S devices='"3"'
+
+dvc exp run --queue \
+    -S agent='ragent' \
+    -S model.path='Qwen/Qwen2.5-7B-Instruct' \
+    -S model.temperature='0.5' \
+    -S model.top_p='0.95' \
+    -S model.few_shot_prob='1.0' \
+    -S retriever.name='hybrid-tei' \
+    -S retriever.top_k='1' \
+    -S repeat='1' \
+    -S run='1' \
+    -S dataset.path='bdsaglam/musique' \
+    -S dataset.name='answerable' \
+    -S dataset.split='validation' \
+    -S devices='"3"'
+
+dvc exp run --queue \
+    -S agent='ragent' \
     -S model.path='bdsaglam/Llama-3.1-8B-Instruct-ragent-grpo-musique-merged' \
     -S model.temperature='0.5' \
     -S model.top_p='0.95' \
@@ -15,6 +76,67 @@ dvc exp run --queue \
     -S devices='"3"'
 
 dvc exp run --queue \
+    -S agent='ragent' \
+    -S model.path='bdsaglam/Llama-3.1-8B-Instruct-ragent-grpo-20250526_110630' \
+    -S model.temperature='0.5' \
+    -S model.top_p='0.95' \
+    -S model.few_shot_prob='1.0' \
+    -S retriever.name='hybrid-tei' \
+    -S retriever.top_k='1' \
+    -S repeat='5' \
+    -S run='1' \
+    -S dataset.path='bdsaglam/musique' \
+    -S dataset.name='answerable' \
+    -S dataset.split='validation' \
+    -S devices='"3"'
+
+dvc exp run --queue \
+    -S agent='ragent' \
+    -S model.path='bdsaglam/Llama-3.1-8B-Instruct-ragent-grpo-20250520_080809-merged' \
+    -S model.temperature='0.5' \
+    -S model.top_p='0.95' \
+    -S model.few_shot_prob='1.0' \
+    -S retriever.name='hybrid-tei' \
+    -S retriever.top_k='1' \
+    -S repeat='5' \
+    -S run='1' \
+    -S dataset.path='bdsaglam/musique' \
+    -S dataset.name='answerable' \
+    -S dataset.split='validation' \
+    -S devices='"3"'
+
+dvc exp run --queue \
+    -S agent='ragent' \
+    -S model.path='bdsaglam/Llama-3.1-8B-Instruct-ragent-grpo-musique-merged' \
+    -S model.temperature='0.5' \
+    -S model.top_p='0.95' \
+    -S model.few_shot_prob='1.0' \
+    -S retriever.name='hybrid-tei' \
+    -S retriever.top_k='1' \
+    -S repeat='5' \
+    -S run='1' \
+    -S dataset.path='bdsaglam/musique' \
+    -S dataset.name='answerable' \
+    -S dataset.split='validation' \
+    -S devices='"3"'
+
+dvc exp run --queue \
+    -S agent='ragent' \
+    -S model.path='Qwen/Qwen2.5-7B-Instruct' \
+    -S model.temperature='0.5' \
+    -S model.top_p='0.95' \
+    -S model.few_shot_prob='1.0' \
+    -S retriever.name='hybrid-tei' \
+    -S retriever.top_k='1' \
+    -S repeat='5' \
+    -S run='1' \
+    -S dataset.path='bdsaglam/musique' \
+    -S dataset.name='answerable' \
+    -S dataset.split='validation' \
+    -S devices='"3"'
+
+dvc exp run --queue \
+    -S agent='ragent' \
     -S model.path='bdsaglam/Llama-3.1-8B-Instruct-ragent-grpo-20250508_213215-merged' \
     -S model.temperature='0.5' \
     -S model.top_p='0.95' \
@@ -29,90 +151,7 @@ dvc exp run --queue \
     -S devices='"3"'
 
 dvc exp run --queue \
-    -S model.path='Qwen/Qwen2.5-7B-Instruct' \
-    -S model.temperature='0.5' \
-    -S model.top_p='0.95' \
-    -S model.few_shot_prob='1.0' \
-    -S retriever.name='hybrid-tei' \
-    -S retriever.top_k='1' \
-    -S repeat='5' \
-    -S run='1' \
-    -S dataset.path='bdsaglam/musique' \
-    -S dataset.name='answerable' \
-    -S dataset.split='validation' \
-    -S devices='"3"'
-
-dvc exp run --queue \
-    -S model.path='meta-llama/Llama-3.1-8B-Instruct' \
-    -S model.temperature='0.5' \
-    -S model.top_p='0.95' \
-    -S model.few_shot_prob='1.0' \
-    -S retriever.name='hybrid-tei' \
-    -S retriever.top_k='1' \
-    -S repeat='5' \
-    -S run='1' \
-    -S dataset.path='bdsaglam/musique' \
-    -S dataset.name='answerable' \
-    -S dataset.split='validation' \
-    -S devices='"3"'
-
-dvc exp run --queue \
-    -S model.path='bdsaglam/Llama-3.1-8B-Instruct-ragent-grpo-musique-merged' \
-    -S model.temperature='0.5' \
-    -S model.top_p='0.95' \
-    -S model.few_shot_prob='1.0' \
-    -S retriever.name='hybrid-tei' \
-    -S retriever.top_k='1' \
-    -S repeat='5' \
-    -S run='1' \
-    -S dataset.path='bdsaglam/musique' \
-    -S dataset.name='answerable' \
-    -S dataset.split='validation' \
-    -S devices='"3"'
-
-dvc exp run --queue \
-    -S model.path='bdsaglam/Llama-3.1-8B-Instruct-ragent-grpo-20250520_080809-merged' \
-    -S model.temperature='0.5' \
-    -S model.top_p='0.95' \
-    -S model.few_shot_prob='1.0' \
-    -S retriever.name='hybrid-tei' \
-    -S retriever.top_k='1' \
-    -S repeat='5' \
-    -S run='1' \
-    -S dataset.path='bdsaglam/musique' \
-    -S dataset.name='answerable' \
-    -S dataset.split='validation' \
-    -S devices='"3"'
-
-dvc exp run --queue \
-    -S model.path='bdsaglam/Llama-3.1-8B-Instruct-ragent-grpo-20250526_110630' \
-    -S model.temperature='0.5' \
-    -S model.top_p='0.95' \
-    -S model.few_shot_prob='1.0' \
-    -S retriever.name='hybrid-tei' \
-    -S retriever.top_k='1' \
-    -S repeat='5' \
-    -S run='1' \
-    -S dataset.path='bdsaglam/musique' \
-    -S dataset.name='answerable' \
-    -S dataset.split='validation' \
-    -S devices='"3"'
-
-dvc exp run --queue \
-    -S model.path='Qwen/Qwen2.5-7B-Instruct' \
-    -S model.temperature='0.5' \
-    -S model.top_p='0.95' \
-    -S model.few_shot_prob='1.0' \
-    -S retriever.name='hybrid-tei' \
-    -S retriever.top_k='1' \
-    -S repeat='1' \
-    -S run='1' \
-    -S dataset.path='bdsaglam/musique' \
-    -S dataset.name='answerable' \
-    -S dataset.split='validation' \
-    -S devices='"3"'
-
-dvc exp run --queue \
+    -S agent='ragent' \
     -S model.path='meta-llama/Llama-3.1-8B-Instruct' \
     -S model.temperature='0.5' \
     -S model.top_p='0.95' \
@@ -127,34 +166,7 @@ dvc exp run --queue \
     -S devices='"3"'
 
 dvc exp run --queue \
-    -S model.path='bdsaglam/Llama-3.1-8B-Instruct-ragent-grpo-20250520_080809-merged' \
-    -S model.temperature='0.5' \
-    -S model.top_p='0.95' \
-    -S model.few_shot_prob='1.0' \
-    -S retriever.name='hybrid-tei' \
-    -S retriever.top_k='1' \
-    -S repeat='1' \
-    -S run='1' \
-    -S dataset.path='bdsaglam/musique' \
-    -S dataset.name='answerable' \
-    -S dataset.split='validation' \
-    -S devices='"3"'
-
-dvc exp run --queue \
-    -S model.path='bdsaglam/Llama-3.1-8B-Instruct-ragent-grpo-20250526_110630' \
-    -S model.temperature='0.5' \
-    -S model.top_p='0.95' \
-    -S model.few_shot_prob='1.0' \
-    -S retriever.name='hybrid-tei' \
-    -S retriever.top_k='1' \
-    -S repeat='1' \
-    -S run='1' \
-    -S dataset.path='bdsaglam/musique' \
-    -S dataset.name='answerable' \
-    -S dataset.split='validation' \
-    -S devices='"3"'
-
-dvc exp run --queue \
+    -S agent='ragent' \
     -S model.path='bdsaglam/Llama-3.1-8B-Instruct-ragent-grpo-20250508_213215-merged' \
     -S model.temperature='0.5' \
     -S model.top_p='0.95' \
