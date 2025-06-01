fa57e901cb59fdf9185d27e4008453db66284ae4
Barış Deniz Sağlam <bdsaglam@gmail.com>
2025-06-01 19:34:52 +0300

    add results

--- nbs/musique-val/run.sh
+++ nbs/musique-val/run.sh
@@ -2,7 +2,7 @@
 
 dvc exp run --queue \
     -S agent='ragent' \
-    -S model.path='bdsaglam/Llama-3.1-8B-Instruct-ragent-grpo-20250520_080809-merged' \
+    -S model.path='bdsaglam/Llama-3.1-8B-Instruct-ragent-grpo-20250531_141657' \
     -S model.temperature='0.5' \
     -S model.top_p='0.95' \
     -S model.few_shot_prob='1.0' \
@@ -13,14 +13,14 @@ dvc exp run --queue \
     -S dataset.path='bdsaglam/musique' \
     -S dataset.name='answerable' \
     -S dataset.split='validation' \
-    -S devices='"3"'
+    -S devices='"0"'
 
 dvc exp run --queue \
     -S agent='ragent' \
-    -S model.path='Qwen/Qwen2.5-7B-Instruct' \
+    -S model.path='bdsaglam/Llama-3.1-8B-Instruct-ragent-grpo-20250531_141657' \
     -S model.temperature='0.5' \
     -S model.top_p='0.95' \
-    -S model.few_shot_prob='1.0' \
+    -S model.few_shot_prob='0.0' \
     -S retriever.name='hybrid-tei' \
     -S retriever.top_k='1' \
     -S repeat='1' \
@@ -28,20 +28,5 @@ dvc exp run --queue \
     -S dataset.path='bdsaglam/musique' \
     -S dataset.name='answerable' \
     -S dataset.split='validation' \
-    -S devices='"3"'
-
-dvc exp run --queue \
-    -S agent='ragent' \
-    -S model.path='bdsaglam/Llama-3.1-8B-Instruct-ragent-grpo-musique-merged' \
-    -S model.temperature='0.5' \
-    -S model.top_p='0.95' \
-    -S model.few_shot_prob='1.0' \
-    -S retriever.name='hybrid-tei' \
-    -S retriever.top_k='1' \
-    -S repeat='1' \
-    -S run='1' \
-    -S dataset.path='bdsaglam/musique' \
-    -S dataset.name='answerable' \
-    -S dataset.split='validation' \
-    -S devices='"3"'
+    -S devices='"1"'
 
