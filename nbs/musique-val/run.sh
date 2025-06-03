96230fe7837aeeec052382c9dc27926e0d0e2345
Barış Deniz Sağlam <bdsaglam@gmail.com>
2025-06-03 17:43:10 +0300

    add more exps

--- nbs/musique-val/run.sh
+++ nbs/musique-val/run.sh
@@ -2,22 +2,7 @@
 
 dvc exp run --queue \
     -S agent='ragent' \
-    -S model.path='Qwen/QwQ-32B' \
-    -S model.temperature='0.5' \
-    -S model.top_p='0.95' \
-    -S model.few_shot_prob='0.0' \
-    -S retriever.name='hybrid-tei' \
-    -S retriever.top_k='1' \
-    -S repeat='1' \
-    -S run='1' \
-    -S dataset.path='bdsaglam/musique' \
-    -S dataset.name='answerable' \
-    -S dataset.split='validation' \
-    -S devices='"0"'
-
-dvc exp run --queue \
-    -S agent='ragent' \
-    -S model.path='Qwen/QwQ-32B' \
+    -S model.path='Qwen/Qwen3-32B' \
     -S model.temperature='0.5' \
     -S model.top_p='0.95' \
     -S model.few_shot_prob='1.0' \
@@ -28,5 +13,20 @@ dvc exp run --queue \
     -S dataset.path='bdsaglam/musique' \
     -S dataset.name='answerable' \
     -S dataset.split='validation' \
-    -S devices='"1"'
+    -S devices='"2,3"'
+
+dvc exp run --queue \
+    -S agent='ragent' \
+    -S model.path='Qwen/Qwen3-32B' \
+    -S model.temperature='0.5' \
+    -S model.top_p='0.95' \
+    -S model.few_shot_prob='0.0' \
+    -S retriever.name='hybrid-tei' \
+    -S retriever.top_k='1' \
+    -S repeat='1' \
+    -S run='1' \
+    -S dataset.path='bdsaglam/musique' \
+    -S dataset.name='answerable' \
+    -S dataset.split='validation' \
+    -S devices='"2,3"'
 
