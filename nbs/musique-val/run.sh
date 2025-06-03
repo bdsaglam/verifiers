34443dc592f943e54980d753e18853bf786b9e22
Barış Deniz Sağlam <bdsaglam@gmail.com>
2025-06-03 12:41:20 +0300

    undo

--- nbs/musique-val/run.sh
+++ nbs/musique-val/run.sh
@@ -1,2 +1,32 @@
 #!/bin/sh
 
+dvc exp run --queue \
+    -S agent='ragent' \
+    -S model.path='Qwen/QwQ-32B' \
+    -S model.temperature='0.5' \
+    -S model.top_p='0.95' \
+    -S model.few_shot_prob='0.0' \
+    -S retriever.name='hybrid-tei' \
+    -S retriever.top_k='1' \
+    -S repeat='1' \
+    -S run='1' \
+    -S dataset.path='bdsaglam/musique' \
+    -S dataset.name='answerable' \
+    -S dataset.split='validation' \
+    -S devices='"0"'
+
+dvc exp run --queue \
+    -S agent='ragent' \
+    -S model.path='Qwen/QwQ-32B' \
+    -S model.temperature='0.5' \
+    -S model.top_p='0.95' \
+    -S model.few_shot_prob='1.0' \
+    -S retriever.name='hybrid-tei' \
+    -S retriever.top_k='1' \
+    -S repeat='1' \
+    -S run='1' \
+    -S dataset.path='bdsaglam/musique' \
+    -S dataset.name='answerable' \
+    -S dataset.split='validation' \
+    -S devices='"1"'
+
